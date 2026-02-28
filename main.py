6c7c7ec6372656961b02f5e3c0013b3bfec2d301
Daniel Bogdanov <danielbogdanov@Daniels-MacBook-Pro-3.local>
2026-02-28 04:04:09 +0200

    first

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+import constants
+from logger import log_state
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(
+        f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}"
+    )
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            screen.fill("black")
+            pygame.display.flip()
+
+            if event.type == pygame.QUIT:
+                return
+
+
+if __name__ == "__main__":
+    main()
